3c86345c897d2e2369e954add29a134996060a39
Dan Doozan <dandoozan@gmail.com>
2013-11-18 11:47:55 -0500

    Initial commit

---
+++ GTFlashcards/gen/com/example/gtflashcards/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.gtflashcards;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ GTFlashcards/src/com/example/gtflashcards/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.gtflashcards;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	/** Called when the user clicks the Login button */
+    public void login(View view) {
+        // Do something in response to button
+    	Intent intent = new Intent(this, LoginActivity.class);
+    	startActivity(intent);
+    }
+
+}
